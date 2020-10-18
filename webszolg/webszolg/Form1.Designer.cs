187ee519b935f5d9613feb9bf07f567a1d463e97
Peti <Peti@DESKTOP-SHEQHLK>
2020-10-18 20:12:10 +0200

    intial commit

---
+++ webszolg/webszolg/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace webszolg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartRatedata = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRatedata)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartRatedata
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartRatedata.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartRatedata.Legends.Add(legend1);
+            this.chartRatedata.Location = new System.Drawing.Point(12, 12);
+            this.chartRatedata.Name = "chartRatedata";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartRatedata.Series.Add(series1);
+            this.chartRatedata.Size = new System.Drawing.Size(300, 300);
+            this.chartRatedata.TabIndex = 0;
+            this.chartRatedata.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chartRatedata);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chartRatedata)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRatedata;
+    }
+}
+
---
+++ webszolg/webszolg/Form1.cs
@@ -0,0 +1,61 @@
+﻿using MySqlX.XDevAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace webszolg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+               
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+rate.Value = value / unit;
+
+                chartRatedata.DataSource = Rates;
+
+                var series = chartRatedata.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRatedata.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRatedata.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
+            }
+        }
+    }
+}
