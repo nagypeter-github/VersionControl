c2ddd5892ac885aee0ef2c8d66c2857eab3cade1
Peti <Peti@DESKTOP-SHEQHLK>
2020-11-15 20:33:29 +0100

    initial commit

---
+++ fejlesztesimintak/fejlesztesimintak/ball.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace fejlesztesimintak
+{
+    public class Ball : Toy
+    {
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+
+
+
+    }
+}
