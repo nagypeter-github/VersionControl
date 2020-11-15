432297006bd0d79d7770d4a0c2849a62c144500d
Peti <Peti@DESKTOP-SHEQHLK>
2020-11-15 20:43:47 +0100

    commit internal

--- fejlesztesimintak/fejlesztesimintak/CarFactory.cs
+++ fejlesztesimintak/fejlesztesimintak/CarFactory.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace fejlesztesimintak
 {
-    public class BallFactory : IToyFactory
+    class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
             return new Ball();
         }
     }
+    
 }
